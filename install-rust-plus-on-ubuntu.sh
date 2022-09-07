fdf89d63ed5b07f212d843d60436f36ad50baf46
stephans <simon.void+git@posteo.net>
2022-09-07 15:33:00 +0200

    fixed the commands and package names

--- install-rust-plus-on-ubuntu.sh
+++ install-rust-plus-on-ubuntu.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-# install openssl support for rust, needed by sccache and when working with openssl in rust projects
-# openssl-dev is the name of the package ssl requires on Ubuntu, but it's different on Fedora (openssl-devel)
-su apt install openssl-dev
-su apt install pgk-config
-
 # let's make sure curl is installed
-su apt install curl
+sudo apt install curl
+# Rust needs a linker
+sudo apt install build-essential
 
-# install rust
+# install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # a better test-runner
@@ -17,6 +14,11 @@ cargo install cargo-nextest --locked
 # linter
 rustup component add clippy
 
+# install openssl support for rust, needed by sccache and when working with openssl in rust projects
+# openssl-dev is the name of the package ssl requires on Ubuntu, but it's different on Fedora (openssl-devel)
+sudo apt install libssl-dev
+sudo apt install pkg-config
+
 # sccache caches compiled libs to speedup compilation
-cargo install sscache
+cargo install sccache
 printf '[build]\nrustc-wrapper = ".cargo/bin/sccache"\n' > ~/.cargo/config.toml
