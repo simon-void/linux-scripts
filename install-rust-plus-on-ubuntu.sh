1c744cdc68277dd599e4435cf0fab94ed4fb8e03
stephans <simon.void@gmx.net>
2022-10-03 22:45:55 +0200

    install and use clang/mold linker over gcc/ldd since it's faster

--- install-rust-plus-on-ubuntu.sh
+++ install-rust-plus-on-ubuntu.sh
@@ -2,20 +2,31 @@
 
 # let's make sure curl is installed
 sudo apt install curl
-# Rust needs a linker
-sudo apt install build-essential
 
 # install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# a better test-runner
-cargo install cargo-nextest --locked
+# Rust needs a linker
+# install mold, since it's the fastest linker, see https://github.com/rui314/mold which requires clang
+sudo apt install mold clang
 
 # install openssl support for rust, needed by sccache and when working with openssl in rust projects
-# openssl-dev is the name of the package ssl requires on Ubuntu, but it's different on Fedora (openssl-devel)
-sudo apt install libssl-dev
-sudo apt install pkg-config
+# libssl-dev is the name of the package ssl requires on Ubuntu, but it's different on Fedora (openssl-devel)
+sudo apt install libssl-dev pkg-config
 
 # sccache caches compiled libs to speedup compilation
 cargo install sccache
-printf '[build]\nrustc-wrapper = ".cargo/bin/sccache"\n' > ~/.cargo/config.toml
+
+# configure global cargo options to always use sccche and mold
+printf '
+[build]
+rustc-wrapper = ".cargo/bin/sccache"
+
+[target.x86_64-unknown-linux-gnu]
+linker = "clang"
+rustflags = ["-C", "link-arg=-fuse-ld=mold"]
+' > ~/.cargo/config.toml
+
+## install useful Rust components
+# a better test-runner
+cargo install cargo-nextest --locked
