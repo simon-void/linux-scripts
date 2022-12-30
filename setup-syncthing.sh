abbe01472bc37bb3d2de85c48b839caa8137f9f6
stephans <simon.void@gmx.net>
2022-12-30 22:43:26 +0100

    adding script setup-syncthing.sh

---
+++ setup-syncthing.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Setting up Syncthing on an ubuntu machine using systemd.
+# Info compiled from https://computingforgeeks.com/how-to-install-and-use-syncthing-on-ubuntu/
+
+# install syncthing
+sudo apt install curl apt-transport-https
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo "deb https://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt-get update
+sudo apt-get install syncthing
+syncthing --version
+
+# setup syncthing as a service started automatically at system start
+
+printf '
+[Unit]
+Description=Syncthing - Open Source Continuous File Synchronization for %I
+Documentation=man:syncthing(1)
+After=network.target
+
+[Service]
+User=%i
+ExecStart=/usr/bin/syncthing -no-browser -gui-address="0.0.0.0:8384" -no-restart -logflags=0
+Restart=on-failure
+SuccessExitStatus=3 4
+RestartForceExitStatus=3 4
+
+[Install]
+WantedBy=multi-user.target
+' > /etc/systemd/system/syncthing@.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable syncthing@$USER
+sudo systemctl start syncthing@$USER
+sudo systemctl status syncthing@$USER
+
+#open syncthing admin-UI in browser
+
+echo "opening syncthing admin-UI"
+echo "first setup a username and password, as should be prompted on the page, also enable 'Use HTTPS for GUI'"
+xdg-open https://localhost:8384/#
+
+
+
