bd29b89f6daa9f2c08f0cff133a5c87bb49fdb08
Schröder, Stephan <stephan.schroeder@gematik.de>
2025-07-08 10:37:58 +0200

    update rust config and ignore log-files (like the once generated by 5ghz-diag.sh)

--- setup-ubuntu/subscripts/setup-rust.sh
+++ setup-ubuntu/subscripts/setup-rust.sh
@@ -2,7 +2,7 @@
 
 echo "###### install rust(up)/cargo"
 
-# install Rust
+# install Rust toolchain
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Rust needs a linker
@@ -28,17 +28,24 @@ rustc-wrapper = ".cargo/bin/sccache"
 [target.x86_64-unknown-linux-gnu]
 linker = "clang"
 rustflags = ["-C", "link-arg=-fuse-ld=mold"]
+
+[net]
+git-fetch-with-cli = true
 ' > ~/.cargo/config.toml
 
 ## install optional Rust addons
 # a better (nicer UI and often faster) test-runner (instead of `cargo test` use `cargo nextest run` to execute your tests)
 cargo install cargo-nextest --locked
 # scans for security vulnerabilities in your dependencies
-cargo install cargo-audit
+# Build your project with dependency lists embedded in the binaries
+# `cargo auditable build --release`
+# Scan the binary for vulnerabilities
+# `cargo audit bin target/release/your-project`
+cargo install cargo-auditable cargo-audit --locked
 # run cargo commands in the background to reduce perceived compilation time
 # e.g. `cargo watch` only triggers `cargo check` which is the default
 # e.g. `cargo watch -x check -x test` triggers first `cargo check` followed by `cargo test`
 # e.g. `cargo watch -x check -s 'cargo nextest run'` triggers first `cargo check` followed by `cargo nextest run`
-cargo install cargo-watch
+cargo install cargo-watch --locked
 # wasm-pack compiles Rust to WASM to interop with js (more on how to use it here: https://rustwasm.github.io/docs/wasm-pack/quickstart.html)
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+cargo install wasm-pack --locked
